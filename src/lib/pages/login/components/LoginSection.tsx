590f5b49c8d2f23fca77d1dd652708fcc97cf31f
highlander <bithighlander@gmail.com>
2022-12-18 23:11:22 -0600

    polyfill madness

--- src/lib/pages/login/components/LoginSection.tsx
+++ src/lib/pages/login/components/LoginSection.tsx
@@ -11,11 +11,99 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+import {
+  useEffect
+} from 'react';
 // import { AiFillGithub } from "react-icons/ai";
 import { useConnectWallet } from "@web3-onboard/react";
+import { v4 as uuidv4 } from 'uuid';
+import { ethers } from 'ethers'
+// @ts-ignore
+import Client from '@pioneer-platform/pioneer-client'
+let spec = 'http://127.0.0.1:9001/spec/swagger.json'
 
 const LoginSection = () => {
   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
+
+  const onLogin = async function(){
+    try{
+      let address = wallet?.accounts[0]?.address
+      console.log("address: ",address)
+
+      let queryKey = localStorage.getItem('queryKey')
+      let username= localStorage.getItem('username')
+      if (!queryKey) {
+        console.log("Creating new queryKey~!")
+        queryKey = 'key:' + uuidv4()
+        localStorage.setItem('queryKey', queryKey)
+      }
+      if (!username) {
+        console.log("Creating new username~!")
+        username = 'user:' + uuidv4()
+        username = username.substring(0, 13);
+        console.log("Creating new username~! username: ", username)
+        localStorage.setItem('username', username)
+      }
+
+      let config = {
+        queryKey,
+        username,
+        spec
+      }
+      console.log("config: ",config)
+      //get config
+      let client = new Client(spec,config)
+      let pioneer = await client.init()
+
+      //is address logged in?
+      let user = await pioneer.GetUser({publicAddress:address})
+      console.log("user: ",user.data)
+
+      //login
+      // let nonce = user.data.nonce
+      // let message = `I am signing my one-time nonce: ${nonce}`
+      //
+      // console.log("wallet: ",wallet)
+      // // console.log("wallet: ",wallet.provider)
+      // // console.log("wallet: ",await wallet.provider.request('personal_sign'))
+      // // console.log("wallet: ",wallet.provider.request(message,address))
+      // // const signature = await wallet.provider.request('personal_sign',{message,address})
+      // // const signature = await wallet.sign(
+      // //     `I am signing my one-time nonce: ${nonce}`,
+      // //     address,
+      // //     '' // MetaMask will ignore the password argument here
+      // // );
+      // // console.log("signature: ",signature)
+      //
+      // console.log("message: ",message)
+      // console.log("address: ",address)
+      //
+      // // const ethersWallet = new ethers.Wallet(wallet.provider)
+      // const ethersProvider = new ethers.providers.Web3Provider(wallet.provider, 'any')
+      // const signer = ethersProvider.getSigner()
+      // let signature = await signer.signMessage(message,address)
+      // console.log("signature: ",signature)
+      // console.log("address: ",address)
+      // console.log("message: ",message)
+      //
+      // //signin get api key
+      // let loginResp = await pioneer.Login({},{publicAddress:address,signature,message})
+      // console.log("loginResp: ",loginResp.data)
+
+      //store api key in localstoarage
+
+
+      return true
+    }catch(e){
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    if(wallet?.accounts && wallet?.accounts[0]?.address){
+      onLogin()
+    }
+  }, [wallet,wallet?.provider])
 
   const onConnect = async function () {
     try {
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,64 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      lib: resolve(__dirname, "src/lib"),
-      routes: resolve(__dirname, "src/routes"),
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      'process.env': {},
+      'process.cwd': {},
+      __APP_ENV__: env.APP_ENV,
     },
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        /^node:.*/,
-        ""
-      ]
-    }
+    plugins: [react()],
+    resolve: {
+      alias: {
+        path: 'rollup-plugin-node-polyfills/polyfills/path',
+        process: 'rollup-plugin-node-polyfills/polyfills/process',
+        lib: resolve(__dirname, "src/lib"),
+        routes: resolve(__dirname, "src/routes"),
+      },
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true
+          }),
+          NodeModulesPolyfillPlugin()
+        ]
+      }
+    },
+    build: {
+      rollupOptions: {
+        external: [
+          /^node:.*/,
+          ""
+        ],
+        plugins: [
+          typescript(),
+          nodePolyfills({ include: null}),
+          commonjs(),
+          // Enable rollup polyfills plugin
+          // used during production bundling
+          rollupNodePolyFill()
+        ]
+      }
+    },
   }
-});
+})
