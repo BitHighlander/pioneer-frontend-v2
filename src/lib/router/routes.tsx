4af71a51518aec52ef02a1cce156fc8e120a0a9f
highlander <bithighlander@gmail.com>
2022-12-19 14:10:28 -0600

    begin dapps page

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -1,6 +1,7 @@
 import type { PathRouteProps } from "react-router-dom";
 
 import Dashboard from "lib/pages/dashboard";
+import Dapps from "lib/pages/dapps";
 import Home from "lib/pages/home";
 import Login from "lib/pages/login";
 
@@ -19,5 +20,21 @@ export const privateRoutes: Array<PathRouteProps> = [
   {
     path: "/dashboard",
     element: <Dashboard />,
+  },
+  {
+    path: "/dapps",
+    element: <Dapps />,
+  },
+  {
+    path: "/blockchains",
+    element: <Dapps />,
+  },
+  {
+    path: "/assets",
+    element: <Dapps />,
+  },
+  {
+    path: "/nodes",
+    element: <Dapps />,
   },
 ];
